694577d333243a07399dd57be11041e7f05b3693
unknown <subhradeepray2017@gmail.com>
2023-03-05 23:21:50 +0530

    Successfully Connected to MongoDB

--- main.go
+++ main.go
@@ -1,10 +1,45 @@
 package main
 
 import (
-	"go.mongodb.org/mongo-driver/mongo"
+	"context"
 	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
+func close(client *mongo.Client, ctx context.Context, cancel context.CancelFunc) {
+	defer cancel()
+	defer func() {
+		if err := client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+}
+
+func connect(uri string) (*mongo.Client, context.Context, context.CancelFunc, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	return client, ctx, cancel, err
+}
+
+func ping(client *mongo.Client, ctx context.Context) error {
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		return err
+	}
+	fmt.Println("Connected Successfully")
+	return nil
+}
+
 func main() {
-	fmt.Println("MongoDB Using Golang")
+	client, ctx, cancel, err := connect("mongodb://localhost:27017")
+	if err != nil {
+		panic(err)
+	}
+
+	defer close(client, ctx, cancel)
+
+	ping(client, ctx)
 }
