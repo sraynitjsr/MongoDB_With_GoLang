3195abdfc543c137f7cfd0b403589fa61fb47fb5
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-03-27 23:57:34 +0530

    Added Routers

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -10,3 +10,15 @@ import (
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "Welcome to API building in Golang\n")
 }
+
+func GetUsers(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome to API building in Golang\n")
+}
+
+func GetUserById(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome to API building in Golang\n")
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome to API building in Golang\n")
+}
--- main.go
+++ main.go
@@ -13,5 +13,8 @@ func main() {
 	fmt.Println("Starting Server")
 	router := httprouter.New()
 	router.GET("/", controllers.Index)
+	router.GET("/users", controllers.GetUsers)
+	router.GET("/users/{userId}", controllers.GetUserById)
+	router.POST("/users/{userId}", controllers.CreateUser)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
